4ea539dcfc7413877a01ce0dcc3eaef8f37e21ae
Laguna <laguna_999@web.de>
2015-08-22 20:13:10 +0200

    Adding EnemyDetector Script

--- Assets/Enemy/Enemy/EnemyDetector.cs
+++ Assets/Enemy/Enemy/EnemyDetector.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class EnemyDetector : MonoBehaviour {
+    
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        this.transform.parent.FindChild("VisorLight").GetComponent<Light>().color = new Color()
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player" )
+        {
+            this.transform.parent.FindChild("VisorLight").GetComponent<Light>().color = new Color(1.0f, 0.1f, 0.1f, 1);
+        }
+    }
+
+    void OnTriggerLeave2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Debug.Log("Set player");
+            this.transform.parent.FindChild("VisorLight").GetComponent<Light>().color = new Color(0.1f, 1.0f, 0.1f, 1);
+
+        }
+    }
+
 }

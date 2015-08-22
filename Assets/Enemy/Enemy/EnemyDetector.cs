8a6cae36fe417d5c4e5700d9afaea68fb15b44c9
Laguna <laguna_999@web.de>
2015-08-22 20:24:57 +0200

    Adding Player Tag, Enemy Visor changes from green to red when they see you.

--- Assets/Enemy/Enemy/EnemyDetector.cs
+++ Assets/Enemy/Enemy/EnemyDetector.cs
@@ -11,19 +11,20 @@ public class EnemyDetector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.parent.FindChild("VisorLight").GetComponent<Light>().color = new Color()
     }
 
     void OnTriggerEnter2D(Collider2D coll)
     {
+        Debug.Log("enter");
         if (coll.gameObject.tag == "Player" )
         {
             this.transform.parent.FindChild("VisorLight").GetComponent<Light>().color = new Color(1.0f, 0.1f, 0.1f, 1);
         }
     }
 
-    void OnTriggerLeave2D(Collider2D coll)
+    void OnTriggerExit2D(Collider2D coll)
     {
+        Debug.Log("leave");
         if (coll.gameObject.tag == "Player")
         {
             Debug.Log("Set player");

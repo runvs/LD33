702ad67d099c8fde354bb8d296816d11de41c022
Laguna <laguna_999@web.de>
2015-08-23 21:54:05 +0200

    Adjusting maxjumpforce

--- Assets/GameProperties.cs
+++ Assets/GameProperties.cs
@@ -7,6 +7,6 @@ public static class GameProperties
     public static float Enemy_WalkTimer = 1.76f;
     public static float Enemy_DetectionTimeMax = 0.75f;
 
-    public static float Player_MaxJumpForce = 1.9f;
+    public static float Player_MaxJumpForce = 1.7f;
     public static float Player_MinJumpForce = 0.5f;
 }

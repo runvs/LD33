7dc48bc4997dfd8bb4adebe2e296240f786dbd73
Thunraz <julian.dinges@gmail.com>
2015-08-23 19:42:37 +0200

    Added light flicker script

---
+++ Assets/Level/FlickerLight.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    public FlickerType FlickerType;
+    public float Frequency = 5.0f;
+    public float MinIntensity = 1f;
+    public float MaxIntensity = 2f;
+
+    private Light _light;
+
+    void Start()
+    {
+        _light = GetComponent<Light>();
+    }
+    
+    void Update()
+    {
+        if(FlickerType == FlickerType.FLICKER)
+        {
+            UpdateFlicker();
+        }
+        else if(FlickerType == FlickerType.PULSE)
+        {
+            UpdatePulse();
+        }
+    }
+
+    private void UpdateFlicker()
+    {
+        var random = Random.Range(0.0f, 1.0f);
+
+        if(random <= Frequency / 100)
+        {
+            _light.intensity = MinIntensity;
+        }
+        else
+        {
+            _light.intensity = MaxIntensity;
+        }
+    }
+    
+    private void UpdatePulse()
+    {
+        _light.intensity = (Mathf.Sin(Time.timeSinceLevelLoad * Frequency) + 1) / 2 * (MaxIntensity - MinIntensity) + MinIntensity;
+    }
+}
+
+public enum FlickerType
+{
+    FLICKER,
+    PULSE
+}

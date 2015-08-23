cd9d518ae790ca8269c5b011a835fb58d573e521
Laguna <laguna_999@web.de>
2015-08-23 14:03:05 +0200

    Adding basic Menu + Button, as well as keeping Camera alive

---
+++ Assets/Scripts/ClearDefaultSceneStuff.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearDefaultSceneStuff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        if (GameObject.FindGameObjectWithTag("DefaultCamera") != null)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("MainCamera"));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

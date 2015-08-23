42a8a4cb740d71b3fadf7d6dbf52a04cab381ab6
Thunraz <julian.dinges@gmail.com>
2015-08-23 14:42:59 +0200

    Added script to menu camera

--- Assets/Level/CameraMovement.cs
+++ Assets/Level/CameraMovement.cs
@@ -2,27 +2,44 @@
 
 public class CameraMovement : MonoBehaviour
 {
-    private Transform Player;
+    private Transform Player = null;
     private float _deadZone = 0.25f;
     private float _speed = 1.5f;
 
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player").transform;;
+        var player = GameObject.FindGameObjectWithTag("Player");
+        
+        if(player != null)
+        {    
+            Player = player.transform;
+        }
     }
 	
     void Update()
     {
-        var playerPosition = Player.position;
-        var cameraPosition = transform.position;
-        cameraPosition.z = playerPosition.z;
-
-        var v3 = playerPosition;
-        v3.z = transform.position.z;
-
-        if (Vector3.Distance(cameraPosition, playerPosition) > _deadZone)
+        if(Player != null)
         {
-            transform.position = Vector3.Lerp(transform.position, v3, _speed * Time.deltaTime);
+            var playerPosition = Player.position;
+            var cameraPosition = transform.position;
+            cameraPosition.z = playerPosition.z;
+    
+            var v3 = playerPosition;
+            v3.z = transform.position.z;
+    
+            if (Vector3.Distance(cameraPosition, playerPosition) > _deadZone)
+            {
+                transform.position = Vector3.Lerp(transform.position, v3, _speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            
+            if(player != null)
+            {    
+                Player = player.transform;
+            }
         }
     }
 }

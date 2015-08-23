2d9973bca1224b2d95686eebb0606d29bd072a26
Laguna <laguna_999@web.de>
2015-08-23 15:14:36 +0200

    fixing bug with duplocated camera in Menu

--- Assets/Scripts/ClearDefaultSceneStuff.cs
+++ Assets/Scripts/ClearDefaultSceneStuff.cs
@@ -6,14 +6,33 @@ public class ClearDefaultSceneStuff : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        // obtain list of gameobjects
         GameObject[] lgo = GameObject.FindGameObjectsWithTag("MainCamera");
         Debug.Log(lgo.Length);
+
+        // check if the camera from the menu is active:
+        bool camFromMenu = false;
         foreach (var go in lgo)
         {
-            if (go.name != "CameraFromMenu")
+            if (go.name == "CameraFromMenu")
             {
-                Destroy(go);
+                camFromMenu = true;
             }
         }
-	}
+
+
+        if (camFromMenu)
+        {
+            foreach (var go in lgo)
+            {
+                if (go.name != "CameraFromMenu")
+                {
+                    Destroy(go);
+                }
+            }
+        }
+
+        lgo = GameObject.FindGameObjectsWithTag("MainCamera");
+        Debug.Log(lgo.Length);
+    }
 }

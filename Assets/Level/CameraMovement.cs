60fbe0fe645642f8bf2cdf4f3792659047376011
Laguna <laguna_999@web.de>
2015-08-24 18:45:30 +0200

    adding hotline miami like camera tilt.

--- Assets/Level/CameraMovement.cs
+++ Assets/Level/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour
     private float _deadZone = 0.25f;
     public float _speed = 1.5f;
 
+    public float _timer = 0;
+
     void Start()
     {
         var player = GameObject.FindGameObjectWithTag("Player");
@@ -18,7 +20,12 @@ public class CameraMovement : MonoBehaviour
 	
     void Update()
     {
-        if(Player != null)
+
+        _timer += Time.deltaTime;
+
+        this.transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(_timer * 1.0f) * 0.75f);
+
+        if (Player != null)
         {
             var playerPosition = Player.position;
             var cameraPosition = transform.position;

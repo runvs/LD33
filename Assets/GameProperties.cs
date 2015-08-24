e2a728ae984e0f0e4bd2b55fd2a352b73f4ff53d
Laguna <laguna_999@web.de>
2015-08-24 18:03:57 +0200

    tutorial should work now in full scale

--- Assets/GameProperties.cs
+++ Assets/GameProperties.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public static class GameProperties
 {
     public static float Enemy_AccelerationForce = 40;
-    public static float Enemy_WalkTimer = 1.76f;
+    public static float Enemy_WalkTimer = 2.0f;
     public static float Enemy_DetectionTimeMax = 0.75f;
 
     public static float Player_MaxJumpForce = 1.7f;
